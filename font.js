1b63819a0ac8fc677abaa99101fe747085c61320
mbelotti <belotti@math.tu-berlin.de>
2023-09-21 09:30:25 +0200

    Making it nice on other devices

--- font.js
+++ font.js
@@ -1,12 +1,12 @@
 function adjustFont(){
 
-    var c=(26)/(1848)
-    var x=window.screen.width
+    var c=(26)/(1848*1044)
+    var x=window.screen.width*window.screen.height
 
-    if (window.screen.width< 1079){
-        document.documentElement.style["font-size"] = 4*c*x
+    if (window.screen.width< 857){
+        document.documentElement.style["font-size"] = 5*c*x
     }else{
-        document.documentElement.style["font-size"] = c*x
+        document.documentElement.style["font-size"] =1.05*Math.sqrt(26*c*x)
     }
 }
 
